a644feb79991ee54e62fdcec3c3b988b317d3e7e
Adam Cooper <amcooper@gmail.com>
2017-01-04 06:30:29 -0500

    Removed comments from routes.js. Removed remoteUrl reference from database.js and added TODO about remote database setup to README. Added some SUBTHIS comments where needed.

--- app/models/item.js
+++ app/models/item.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var itemSchema = new mongoose.Schema({
-	title: String,
+	title: String, // SUBTHIS and below
 	attr01: String,
 	attr02: String,
 	attr03: String
--- app/routes.js
+++ app/routes.js
@@ -1,43 +1,33 @@
-var Item = require('./models/item'); // SUBTHIS
+var Item = require('./models/item');
 
-function getItems(res) { // SUBTHIS
-    Item.find(function (err, items) { // SUBTHIS
-
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+function getItems(res) {
+    Item.find(function (err, items) {
         if (err) {
             res.send(err);
         }
-
-        res.json(items); // return all items in JSON format // SUBTHIS
+        res.json(items); 
     });
 };
 
 module.exports = function (app) {
-
-    // api ---------------------------------------------------------------------
-    // get all items
-    app.get('/api/items', function (req, res) { // SUBTHIS
-        // use mongoose to get all items in the database
-        getItems(res); // SUBTHIS
+    app.get('/api/items', function (req, res) {
+        getItems(res);
     });
 
-    // get a single item // SUBTHIS
-    app.get('/api/items/:item_id', function (req, res) { // SUBTHIS
-        Item.findById(req.params.item_id, function (err, item) { // SUBTHIS
+    app.get('/api/items/:item_id', function (req, res) {
+        Item.findById(req.params.item_id, function (err, item) {
             if (err)
                 res.send(err);
-
             res.json(item);
         });
     });
 
-    // edit a single item
     app.put('/api/items/:item_id', function (req, res) {
         Item.findById(req.params.item_id, function (err, item) {
             if (err) {
                 res.send(err);
             } else {
-                item.title = req.body.title;
+                item.title = req.body.title; // SUBTHIS and below
                 item.attr01 = req.body.attr01;
                 item.attr02 = req.body.attr02;
                 item.attr03 = req.body.attr03;
@@ -48,46 +38,32 @@ module.exports = function (app) {
         });
     });
 
-    // create item and send back all items after creation
-    app.post('/api/items', function (req, res) { // SUBTHIS
-
-        // create an item, information comes from AJAX request from Angular
-        Item.create({ // SUBTHIS
-            title: req.body.title,
+    app.post('/api/items', function (req, res) {
+        Item.create({
+            title: req.body.title, // SUBTHIS and below
             attr01: req.body.attr01,
             attr02: req.body.attr02,
             attr03: req.body.attr03,
             done: false
-        }, function (err, item) { // SUBTHIS
+        }, function (err, item) {
             if (err)
                 res.send(err);
-
             res.json(item);
-
-            // Old version:
-            // get and return all the items after you create another
-            // getItems(res); // SUBTHIS
-        });
-
-    });
-
-    // delete an item
-    app.delete('/api/items/:item_id', function (req, res) { // SUBTHIS
-        Item.remove({ // SUBTHIS
-            _id: req.params.item_id // SUBTHIS
-        }, function (err, item) { // SUBTHIS
-            if (err)
-                res.send(err);
-
-            res.json(item);
-
-            // Old version:
-            // getItems(res); // SUBTHIS
         });
     });
 
-    // application -------------------------------------------------------------
+    app.delete('/api/items/:item_id', function (req, res) {
+        Item.remove({
+            _id: req.params.item_id
+        }, function (err, item) {
+            if (err)
+                res.send(err);
+            res.json(item);
+
+        });
+    });
+
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '/public/index.html'); 
     });
 };
